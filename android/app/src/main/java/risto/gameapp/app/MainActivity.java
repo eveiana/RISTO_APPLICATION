814e709e8a21d24125d1cc2f2e073628533e5892
eveiana <evalineatieno857@gmail.com>
2026-06-10 12:40:35 +0300

    Add files via upload

---
+++ android/app/src/main/java/risto/gameapp/app/MainActivity.java
@@ -0,0 +1,5 @@
+package risto.gameapp.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ android/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'risto.game.app',
+  appName: 'risto',
+  webDir: 'www'
+};
+
+export default config;
